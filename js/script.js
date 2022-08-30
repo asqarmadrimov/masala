b88682c903325827c5c92f70d3ece38419ddda56
asqarmadrimov <madrimov2530@gmail.com>
2022-08-30 15:10:29 +0500

    site

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// 1-masala
+// number(5,10)
+// function number(integer, limit) {
+  
+//   const arr = [];
+  
+//   for (let i = 1; i <= limit; i ++) {
+//       if (integer * i) {
+//         arr.push(integer * i);
+//       }
+//   }
+//   console.log(arr);
+//   return arr;
+// }
+
+
+
+// 2-masala
+// test(2,33)
+
+// function test(a, b) {
+
+//   let arr = [];
+
+//   for(let i = a; i < b; i++) {
+//    arr.push(i)
+//   }
+   
+//   console.log(arr);
+//   return arr
+// }
+
+
+
+// 3-masala
+
